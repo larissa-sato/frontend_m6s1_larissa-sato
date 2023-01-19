541a876c1ed7847e4789cdf8c4173a28410e4f54
larissa-sato <larissasato_@outlook.com>
2023-01-19 20:14:13 -0300

    fixed ValueInfos conditional

--- src/components/ValueInfos/index.tsx
+++ src/components/ValueInfos/index.tsx
@@ -6,20 +6,36 @@ import { CalculateContext } from '../../context/calculateContext';
 const DivValue = () => {
     
     const {values} = useContext(CalculateContext)
-
     let newValue = Object.values(values)
-
-      
+         
     return(
         <ValueDiv>
-            <u>
-            <h2>VOCÊ RECEBERÁ:</h2>
-            </u>
-
-            <li>Amanhã: R$ {`${newValue[0]}`}</li>
-            <li>Em 15 dias: R$ {`${newValue[1]}`}</li>
-            <li>Em 30 dias: R$ {`${newValue[2]}`}</li>
-            <li>Em 90 dias: R$ {`${newValue[3]}`}</li>   
+            <>
+            {newValue.length > 0 ? (
+                <>
+                <u>
+                <h2>VOCÊ RECEBERÁ:</h2>
+                </u>
+    
+                <li>Amanhã: R$ {`${newValue[0]}`},00</li>
+                <li>Em 15 dias: R$ {`${newValue[1]}`},00</li>
+                <li>Em 30 dias: R$ {`${newValue[2]}`},00</li>
+                <li>Em 90 dias: R$ {`${newValue[3]}`},00</li>   
+            </>
+            ) : (
+            <>
+                <u>
+                <h2>VOCÊ RECEBERÁ:</h2>
+                </u>
+    
+                <li>Amanhã: R$ 0,00</li>
+                <li>Em 15 dias: R$ 0,00</li>
+                <li>Em 30 dias: R$ 0,00</li>
+                <li>Em 90 dias: R$ 0,00</li>   
+            </>
+            )
+            }
+            </>
         </ValueDiv>
     )
 };
